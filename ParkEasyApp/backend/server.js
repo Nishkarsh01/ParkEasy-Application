c5173ea1bb3df0a42de919defc9abeb652141d9f
Nishkarsh01 <nishdubb20@gmail.com>
2024-10-11 04:25:59 -0400

    add deployed code

--- ParkEasyApp/backend/server.js
+++ ParkEasyApp/backend/server.js
@@ -9,9 +9,25 @@ require('./services/AuthService');  // Initialize Passport.js
 
 const app = express();
 
+// Set up the allowed origins including the Ngrok URL for development
+const allowedOrigins = [
+  'https://localhost:3000',  // Localhost for local development
+  'https://parkeasy-application.onrender.com',  // Your production URL
+  'https://1002-172-97-138-16.ngrok-free.app'  // Ngrok URL for development
+];
+
+// CORS configuration to allow requests from specific origins
 app.use(cors({
-  origin: ['https://localhost:3000', 'https://parkeasy-application.onrender.com'],  // Adjust these URLs as per your environment
-  credentials: true
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps, curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true  // Allow credentials such as cookies or authorization headers
 }));
 
 const port = process.env.PORT || 3000;
